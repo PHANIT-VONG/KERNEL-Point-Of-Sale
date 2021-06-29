38832216bb69469e37593cd753ee855754bf4258
PHANIT-VONG <vphanit2020@gmail.com>
2021-06-29 11:49:26 +0700

    Kernel update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
-
 import 'package:connectivity/connectivity.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:point_of_sale/bloc_order/bloc_order.dart';
 import 'package:point_of_sale/bloc_theme/bloc_theme.dart';
 import 'package:point_of_sale/screen/home_screen.dart';
